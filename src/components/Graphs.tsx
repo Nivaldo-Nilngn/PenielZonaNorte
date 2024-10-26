46e4e3000f9a7209318576088e9db4e69c3b64c1
NIVALDO G NETO <158115481+Nivaldo-Nilngn@users.noreply.github.com>
2024-10-26 00:26:34 -0300

    Atualizar o Graphs.tsx

--- src/components/Graphs.tsx
+++ src/components/Graphs.tsx
@@ -183,19 +183,18 @@ const Graphs = ({ items }: Props) => {
 // Estilização
 const Container = styled.div`
   margin: 20px;
-  text-align: center;  // Centraliza os gráficos
+  text-align: center;
 `;
 
 const StyledButton = styled.button`
   background-color: #2980b9;
-  color: white; /* Cor do texto */
-  border: none; /* Remove a borda */
-  border-radius: 5px; /* Bordas arredondadas */
-  padding: 10px 20px; /* Espaçamento interno */
-  font-size: 16px; /* Tamanho da fonte */
-  cursor: pointer; /* Cursor pointer ao passar o mouse */
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
 
-  /* Efeito de hover */
   &:hover {
     background-color: #1a5276;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
@@ -204,29 +203,29 @@ const StyledButton = styled.button`
 
 const GraphsContainer = styled.div`
   display: flex;
-  justify-content: center;  // Centraliza os gráficos no desktop
-  flex-direction: column;   // No mobile, empilha os gráficos
-  align-items: center; // Centraliza verticalmente
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   @media(min-width: 768px) {
-    flex-direction: row;  // No desktop, exibe os gráficos lado a lado
+    flex-direction: row;
   }
 `;
 
 const GraphSection = styled.div`
   width: 100%;
-  max-width: 400px;  // Limita o tamanho máximo do gráfico
-  margin: 20px;  // Centraliza os gráficos sem usar auto
+  max-width: 400px;
+  margin: 20px;
   text-align: center;
 `;
 
 const TableContainer = styled.div`
   margin-top: 20px;
-  width: 100%; // Permite que a tabela ocupe a largura total
-  max-width: 600px; // Limita a largura máxima da tabela
-  margin-left: auto; // Centraliza horizontalmente
-  margin-right: auto; // Centraliza horizontalmente
-  display: flex; // Adicione isso para garantir que a tabela ocupe espaço
-  justify-content: center; // Centraliza a tabela
+  width: 100%;
+  max-width: 600px;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  justify-content: center;
 `;
 
 const StyledTable = styled.table`
@@ -243,7 +242,7 @@ const StyledTable = styled.table`
     background-color: #f2f2f2;
   }
 
-tr:nth-child(even) {
+  tr:nth-child(even) {
     background-color: #f9f9f9;
   }
 
